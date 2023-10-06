8e4b38a9a7b8f698122c0914342b42a0d741ceac
Rusya575 <78809194+Rusya575@users.noreply.github.com>
2023-10-06 23:45:05 +0300

    Update sem3_zad8.py

--- sem3_zad8.py
+++ sem3_zad8.py
@@ -1,12 +1,12 @@
 a = int(input())
 n = list(map(int, input().split()))
 s = 0
-for i in range(max(s)):
+for i in range(max(n)):
     for j in range(a):
-        if min(a) + i == a[j]:
+        if min(n) + i == n[j]:
             s += 1
             break
-    if s == n//2+1:
+    if s == a//2+1:
         k = i
         break
-print(min(a) + k)
+print(min(n) + k)
